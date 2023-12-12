c937823a794b75f16cbe483b26b1e8bdf985e07e
Sudhanshu Purohit <purohitsudhanshu294@gmail.com>
2023-12-12 18:28:53 +0530

    bottom nav bar added

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,12 +1,22 @@
+import 'package:ether_app/core/constants/constants.dart';
 import 'package:ether_app/features/auth/controller/auth_controller.dart';
 import 'package:ether_app/features/home/delegates/search_community_delegate.dart';
 import 'package:ether_app/features/home/drawers/community_list_drawer.dart';
 import 'package:ether_app/features/home/drawers/profile_drawer.dart';
+import 'package:ether_app/theme/pallete.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+
+  int _page = 0;
 
   void displayDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
@@ -16,9 +26,16 @@ class HomeScreen extends ConsumerWidget {
     Scaffold.of(context).openEndDrawer();
   }
 
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final user = ref.watch(userProvider)!;
+    final currentTheme = ref.watch(themeNotifierProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -54,8 +71,22 @@ class HomeScreen extends ConsumerWidget {
       ),
       drawer: const CommunityListDrawer(),
       endDrawer: const ProfileDrawer(),
-      body: Center(
-        child: Text(user.name),
+      body: Constants.tabWidgets[_page],
+      bottomNavigationBar: CupertinoTabBar(
+        activeColor: currentTheme.iconTheme.color,
+        backgroundColor: currentTheme.backgroundColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add post'
+          )
+        ],
+        onTap: onPageChanged,
+        currentIndex: _page,
       ),
     );
   }
